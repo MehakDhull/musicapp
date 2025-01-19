caaa455a693c730143a208a158154d42619c81b3
Mehak Dhull <mehakdhull189@gmail.com>
2025-01-19 16:08:54 -0500

    Initial commit

---
+++ js/artists.js
@@ -0,0 +1,63 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+
+window.artists = [
+    {
+      artistId: "AID-1",
+      name: "Diljit Dosanjh",
+      urls: [
+        { url: "https://en.wikipedia.org/wiki/Diljit_Dosanjh", name: "Wikipedia" },
+        { url: "https://www.instagram.com/diljitdosanjh/?hl=en", name: "Instagram" }
+      ],
+      imageUrl: "./imageURL/Diljit-Dosanjh.webp"
+      
+    },
+    {
+      artistId: "AID-2",
+      name: "Taylor Swift",
+      urls: [
+        { url: "https://www.taylorswift.com/", name: "Official Website" },
+        { url: "https://www.instagram.com/taylorswift/", name: "Instagram" },
+        { url: "https://x.com/taylorswift13", name: "Twitter" },
+        { url: "https://en.wikipedia.org/wiki/Taylor_Swift", name: "Wikipedia" }
+      ],
+      imageUrl: "./imageURL/taylorswift.webp"
+    },
+    {
+      artistId: "AID-3",
+      name: "Sabrina Carpenter",
+      urls: [
+        { url: "https://www.sabrinacarpenter.com/", name: "Official Website" },
+        { url: "https://www.instagram.com/sabrinacarpenter/", name: "Instagram" },
+        { url: "https://x.com/sabrinaannlynn", name: "Twitter" },
+        { url: "https://en.wikipedia.org/wiki/Sabrina_Carpenter", name: "Wikipedia" }
+      ],
+      imageUrl: "./imageURL/sabrina.png"
+    },
+    {
+      artistId: "AID-4",
+      name: "Arijit Singh",
+      urls: [
+        { url: "https://www.instagram.com/arijitsingh/?hl=en", name: "Instagram" },
+        { url: "https://x.com/arijitsingh?lang=en", name: "Twitter" },
+        { url: "https://en.wikipedia.org/wiki/Arijit_Singh", name: "Wikipedia" }
+      ],
+      imageUrl: "./imageURL/arijit sing.webp"
+    },
+    {
+      artistId: "AID-5",
+      name: "AP Dhillon",
+      urls: [
+       
+        { url: "https://www.instagram.com/apdhillon/?hl=en", name: "Instagram" },
+        { url: "https://x.com/apdhillxn/status/1324813936558923776", name: "Twitter" },
+        { url: "https://en.wikipedia.org/wiki/AP_Dhillon", name: "Wikipedia" }
+      ],
+      imageUrl: "./imageURL/ap dhillion.jpg"
+    },
+  ];
+  
---
+++ js/songs.js
@@ -0,0 +1,243 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+
+window.songs = [
+    // Diljit Dosanj Songs 
+    {
+      songId: "SongID-101",
+      artistId: "AID-1",
+      title: "Naina(From Crew)",
+      year: "2024",
+      duration: 180, // 3:00 in seconds
+      url: "https://open.spotify.com/track/1eZefeDb8uOsjvcbl1fJrG",
+      imageUrl: "./imageURL/naina.webp"
+    },
+    {
+      songId: "SongID-102",
+      artistId: "AID-1",
+      title: "Kinni Kinni ",
+      year: "2023",
+      duration: 198, // 3:33 in seconds
+      url: "https://open.spotify.com/track/3F3ZuXYFXGM1V6pk3b3zmi",
+      imageUrl: "./imageURL/kinni.jpeg"
+    },
+    {
+      songId: "SongID-103",
+      artistId: "AID-1",
+      title: "Bad Habits",
+      year: "2023",
+      duration: 120, // 2:47 in seconds
+      url: "https://open.spotify.com/track/2CHMiZynW2WhXSizjcs9R5",
+      imageUrl: "./imageURL/Diljit-Dosanjh.webp"
+    },
+    {
+      songId: "SongID-104",
+      artistId: "AID-1",
+      title: "Hass Hass",
+      year: "2023",
+      duration: 120, // 2:33 in seconds
+      url: "https://open.spotify.com/track/72vuBPMhwFNlSYpTSf6fVD",
+      imageUrl: "./imageURL/has-has.jpg"
+    },
+    {
+      songId: "SongID-105",
+      artistId: "AID-1",
+      title: "Raat Di Gedi",
+      year: "2018",
+      duration: 180, // 3:18 in seconds
+      url: "https://open.spotify.com/track/52rRjGjdTwlDIKXjTLKAv9",
+       imageUrl: "./imageURL/Diljit-Dosanjh.webp"
+    },
+  
+    // Taylor Swift
+    {
+      songId: "SongID-106",
+      artistId: "AID-2",
+      title: "Fortight(feat.PostMlone)",
+      year: "2024",
+      duration: 180, // 3:48 in seconds
+      url: "https://open.spotify.com/track/2OzhQlSqBEmt7hmkYxfT6m",
+       imageUrl: "./imageURL/fortniht.jpg"
+    },
+    {
+      songId: "SongID-107",
+      artistId: "AID-2",
+      title: "Cruel Summer",
+      year: "2018",
+      duration: 120, // 2:58 in seconds
+      url: "https://open.spotify.com/track/1BxfuPKGuaTgP7aM0Bbdwr",
+       imageUrl: "./imageURL/cruelsummer.jpeg"
+    },
+    {
+      songId: "SongID-108",
+      artistId: "AID-2",
+      title: "Gorgeous",
+      year: "2017",
+      duration: 180, // 3:29 in seconds
+      url: "https://open.spotify.com/track/1ZY1PqizIl78geGM4xWlEA",
+      imageUrl: "./imageURL/gorgeour.jpeg"
+    },
+    {
+      songId: "SongID-109",
+      artistId: "AID-2",
+      title: "Delicate",
+      year: "2017",
+      duration: 180, // 3:52 in seconds
+      url: "https://open.spotify.com/track/6NFyWDv5CjfwuzoCkw47Xf",
+       imageUrl: "./imageURL/taylorswift.webp"
+    },
+    {
+      songId: "SongID-110",
+      artistId: "AID-2",
+      title: "End Game",
+      year: "2017",
+      duration: 240, // 4:04 in seconds
+      url: "https://open.spotify.com/track/2x0WlnmfG39ZuDmstl9xfX",
+      imageUrl: "./imageURL/fortniht.jpg"
+    },
+  
+    // Sabrina Carpenter
+    {
+      songId: "SongID-111",
+      artistId: "AID-3",
+      title: "Espresso",
+      year: "2024",
+      duration: 120, // 2:55 in seconds
+      url: "https://open.spotify.com/artist/74KM79TiuVKeVCqs8QtB0B",
+      imageUrl: "./imageURL/espresso.png"
+    },
+    {
+      songId: "SongID-112",
+      artistId: "AID-3",
+      title: "Nonsense",
+      year: "2022",
+      duration: 120, // 2:43 in seconds
+      url: "https://open.spotify.com/track/6dgUya35uo964z7GZXM07g",
+     imageUrl: "./imageURL/nonsense.jpeg"
+    },
+    {
+      songId: "SongID-113",
+      artistId: "AID-3",
+      title: "Please Please Please",
+      year: "2024",
+      duration: 183, // 3:06 in seconds
+      url: "https://open.spotify.com/track/5N3hjp1WNayUPZrA8kJmJP",
+      imageUrl: "./imageURL/pleaseplease.jpeg"
+    },
+    {
+      songId: "SongID-114",
+      artistId: "AID-3",
+      title: "Opposite",
+      year: "2023",
+      duration: 123, // 2:48 in seconds
+      url: "https://open.spotify.com/track/27Jko74ZSVY1kwt0vMY6Pu",
+      imageUrl: "./imageURL/opposite.jpg"
+    },
+    {
+      songId: "SID-115",
+      artistId: "AID-3",
+      title: "Already Over",
+      year: "2022",
+      duration: 128, // 2:50 in seconds
+      url: "https://open.spotify.com/track/5QhJorJmjqYfETfqgBtGg3",
+      imageUrl: "./imageURL/sabrina.png"
+    },
+  
+    // Arijit Singh
+    {
+      songId: "SongID-116",
+      artistId: "AID-4",
+      title: "Soulmate",
+      year: "2024",
+      duration: 190, // 3:33 in seconds
+      url: "https://open.spotify.com/track/1tf2iody5iRLYYPDSBpzRc",
+      imageUrl: "./imageURL/soulmate.jpg"
+    },
+    {
+      songId: "SongID-117",
+      artistId: "AID-4",
+      title: "Hawayein",
+      year: "2024",
+      duration: 240, // 4:49 in seconds
+      url: "https://open.spotify.com/track/7COtUHLKtQk3ZS9UODELIm",
+   imageUrl: "./imageURL/arijit sing.webp"
+    },
+    {
+      songId: "SongID-118",
+      artistId: "AID-4",
+      title: "Heeriye",
+      year: "2023",
+      duration: 189, // 3:14 in seconds
+      url: "https://open.spotify.com/track/5PUXKVVVQ74C3gl5vKy9Li",
+      imageUrl: "./imageURL/heeriye.jpeg"
+    },
+    {
+      songId: "SongID-119",
+      artistId: "AID-4",
+      title: "Samjhawan",
+      year: "2024",
+      duration: 248, // 4:47 in seconds
+      url: "https://open.spotify.com/track/0OpK6yAPijNR43mAHmxbQY",
+    imageUrl: "./imageURL/samjhawan.jpeg"
+    },
+    {
+      songId: "SongID-120",
+      artistId: "AID-4",
+      title: "Enna Sona",
+      year: "2024",
+      duration: 181, // 3:33 in seconds
+      url: "https://open.spotify.com/track/21yRtB6B8EMounImAfHRCP?si=af1664482d944644",
+      imageUrl: "./imageURL/enna-sona.jpeg"
+    },
+    // AP Dhillon
+    {
+      songId: "SongID-121",
+      artistId: "AID-5",
+      title: "Excuses",
+      year: "2023",
+      duration: 120, // 3:42 in seconds
+      url: "https://open.spotify.com/track/0xIuPDzJSnJywALez8dwKR",
+      imageUrl: "./imageURL/excuses.jpeg"
+    },
+    {
+      songId: "SongID-122",
+      artistId: "AID-5",
+      title: "Tere Te",
+      year: "2021",
+      duration: 60, // 1:54 in seconds
+      url: "https://open.spotify.com/track/0ptqV9ySE8HVbWblGdv3Eb",
+      imageUrl: "./imageURL/terete.jpg"
+    },
+    {
+      songId: "SongID-123",
+      artistId: "AID-5",
+      title: "Dil Nu",
+      year: "2022",
+      duration: 198, // 3:53 in seconds
+      url: "https://open.spotify.com/track/7AsG1F1eMWd17RYan8M3Wm",
+     imageUrl: "./imageURL/dilnu.jpeg"
+    },
+    {
+      songId: "SongID-124",
+      artistId: "AID-5",
+      title: "Wo Noor",
+      year: "2022",
+      duration: 205, // 3:25 in seconds
+      url: "https://open.spotify.com/track/32FVH23yFXr8Z0sfjRw4It",
+     imageUrl: "./imageURL/wonoor.jpeg"
+    },
+    {
+      songId: "SongID-125",
+      artistId: "AID-5",
+      title: "Desries",
+      year: "2021",
+      duration: 195, // 2:48 in seconds
+      url: "https://open.spotify.com/track/5o2FwTc46RceaBp3B2ZjNf",
+  imageUrl: "./imageURL/ap dhillion.jpg"
+    }
+  ];
+  
